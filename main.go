5bc01816981324c66c32c59390dc87755264c68f
Azur冰弦 <973562770@qq.com>
2023-10-04 13:20:30 +0800

    doc: comment of go:embed in main.go

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"log"
 )
 
-// TODO:这里会报错
-// //go:embed assets/build/*
+
+// go1.16后新加入的功能，将文件或目录作为一个文件系统嵌入到二进制文件中
+// 这里就是将前端的构建产物目录嵌入到了二进制文件中
+// 所以如果这里报错，大概是没有构建前端，目录不存在
+//go:embed assets/build/*
 var f embed.FS
 
 func init() {
